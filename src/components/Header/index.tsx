14d84952a8ceae77975b04d2792aa862738212a8
Carlos Mayrinck <carlosmayrinck1@gmail.com>
2023-01-17 00:22:35 -0300

    logout button added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from "./styles";
 import { useAuth } from "../../hooks/useAuth";
 import { useEffect, useState } from "react";
+import { CiLogout } from "react-icons/ci";
 
 interface UserData {
   token: string;
@@ -16,20 +17,21 @@ interface UserData {
 export function Header() {
 
   const [userData, setUserData] = useState<UserData>({} as UserData);
-  const { loggedIn } = useAuth();
+  const { loggedIn, signOut } = useAuth();
 
   useEffect(() => {
     if( loggedIn ) {
       const user = JSON.parse(localStorage.getItem("@indications:auth_user_data") ?? "");
       setUserData(user);
     }
-  }, []);
+  }, [loggedIn]);
 
   return (
     <Container>
       <nav>
 
         <span>{ userData.user?.name }</span>
+        <button type="button" onClick={signOut}><CiLogout /> <span>Sair</span></button>
       </nav>
     </Container>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,8 +9,30 @@ export const Container = styled.header`
   justify-content: end;
   background: var(--shape);
 
-  span {
-    font-weight: 700;
-    color: var(--white);
+  nav {
+    display: flex;
+    align-items: center;
+
+    > span {
+      font-weight: 700;
+      color: var(--white);
+      margin-right: 1rem;
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+      border: none;
+      background: transparent;
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 2rem;
+      color: var(--white);
+      margin-left: 1rem;
+
+      span {
+        margin-left: 0.5rem;
+      }
+    }
   }
 `;
